28a3b90fe67a61eb2e588ecf73b0333a68b53dac
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-27 03:15:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -43,6 +43,7 @@ def apply_coupons(cart, coupons)
    if cart_item && cart_item[:count] >= coupons[i][:num]
      if cart_item_w_coupon
        cart_item_w_coupon += coupons[i][:num]
+       cart_item[:count] -= 
      end
    end
     i +=1
