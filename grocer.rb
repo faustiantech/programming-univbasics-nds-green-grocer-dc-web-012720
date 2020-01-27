7ccd9d30485ddc802af733b659c57cbd3617fadd
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-27 03:21:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,7 +48,7 @@ def apply_coupons(cart, coupons)
        cart_item_w_coupon = {
          :item => discount_item_name,
          :price => coupons[i][:cost] / coupons[i][:num],
-         :count = coupons[i][:num],
+         :count => coupons[i][:num],
          :clearence => cart_item[:clearence]
        }
        cart.push(cart_item_w_coupon)
