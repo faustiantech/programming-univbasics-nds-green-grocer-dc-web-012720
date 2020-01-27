c7d17a1e5dd62da776182e515f50a542ac870339
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-27 03:11:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,7 +37,7 @@ def apply_coupons(cart, coupons)
   i=0
   while i < coupons.length do
    cart_item = find_item_by_name_in_collection(coupons[i][:item],cart)
-   
+   discount_item_name = " "
     i +=1
   end
 end
