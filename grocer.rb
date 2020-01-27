dd32b4e361ccfc3581d61a426e05b3b432946705
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-27 03:19:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def apply_coupons(cart, coupons)
          :clearence => cart_item[:clearence]
        }
        cart.push(cart_item_w_coupon)
-       cart_item[:count] -= coupons[i]
+       cart_item[:count] -= coupons[i][:num]
      end
    end
     i +=1
